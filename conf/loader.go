e4a1082c58f28d816ef56e2856c514defed4c14b
vizoss <think22026@gmail.com>
2023-12-09 13:57:25 +0800

    update

--- conf/loader.go
+++ conf/loader.go
@@ -6,38 +6,59 @@ import (
 	"os"
 )
 
-var consulEndpoint = flag.String("config-consul-endpoint", "", "consul address")
-var consulKey = flag.String("config-consul-key", "", "consul key")
-var configFile = flag.String("config-file", "etc/server.yaml", "the config file")
+var configConsulEndpoint = flag.String("config-consul-endpoint", "", "consul address")
+var configConsulKey = flag.String("config-consul-key", "", "consul key")
+var configPath = flag.String("config-path", "etc/server.yaml", "the config file")
 
 func init() {
 	flag.Parse()
 }
 
-func getConsul() (endpoint, key string) {
-	if *consulEndpoint != "" && *consulKey != "" {
-		return *consulEndpoint, *consulKey
+func getConfigConsul() (endpoint, key string) {
+	if *configConsulEndpoint != "" && *configConsulKey != "" {
+		return *configConsulEndpoint, *configConsulKey
 	} else {
 		return os.Getenv("config-consul-endpoint"), os.Getenv("config-consul-key")
 	}
 }
 
-func LoadConfig() *Config {
+func getConfigPath() string {
+	if *configPath != "" {
+		return *configPath
+	} else {
+		return os.Getenv("config-path")
+	}
+}
+
+func LoadConfig(path string) *Config {
 	var (
 		config Config
 		err    error
 	)
-	cAddress, cKey := getConsul()
-	if cAddress != "" && cKey != "" {
-		config, err = LoadFromConsul(cAddress, cKey)
+	consulAddress, consulKey := getConfigConsul()
+	if consulAddress != "" && consulKey != "" {
+		config, err = LoadFromConsul(consulAddress, consulKey)
 		if err != nil {
 			panic(fmt.Sprintf("config read error: %v", err))
 		}
-	} else {
-		config, err = Load(*configFile)
-		if err != nil {
-			panic(fmt.Sprintf("config read error: %v", err))
-		}
+		return &config
 	}
-	return &config
+
+	confPath := getConfigPath()
+	if confPath != "" {
+		config, err = Load(confPath)
+		if err != nil {
+			panic(fmt.Sprintf("config read error: %v", err))
+		}
+		return &config
+	}
+
+	if path != "" {
+		config, err = Load(path)
+		if err != nil {
+			panic(fmt.Sprintf("config read error: %v", err))
+		}
+		return &config
+	}
+	panic("load config error")
 }
