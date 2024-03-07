cc7883d3f1b09b8a5c7e2aafdd1fa60a40fb26a3
vizoss <think220216@gmail.com>
2024-03-07 21:10:35 +0800

    add error code

--- errorx/errorxs.go
+++ errorx/errorxs.go
@@ -4,6 +4,7 @@ var (
 	ErrParamsError         = NewErrorX(4000000, "Params Error")
 	ErrPermission          = NewErrorX(4000001, "Permission denied")
 	ErrNotSupportReCommit  = NewErrorX(4000002, "Not support Recommit")
+	ErrNotFound            = NewErrorX(4000003, "Not Found")
 	ErrInternalServerError = NewErrorX(5000000, "Internal Server err")
 	ErrServerBusy          = NewErrorX(5000001, "Server busy")
 )
