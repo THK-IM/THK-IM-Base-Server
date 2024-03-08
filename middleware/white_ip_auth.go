00a364499d12dd05c333ad0d6a1df55863ec2ff0
vizoss <think220216@gmail.com>
2024-03-08 20:30:26 +0800

    clean code

--- middleware/white_ip_auth.go
+++ middleware/white_ip_auth.go
@@ -4,21 +4,23 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
 	"github.com/thk-im/thk-im-base-server/dto"
+	"github.com/thk-im/thk-im-base-server/server"
 	"net"
 	"strings"
 )
 
-func WhiteIpAuth(ipWhiteList string, logger *logrus.Entry) gin.HandlerFunc {
+func WhiteIpAuth(appCtx *server.Context) gin.HandlerFunc {
+	ipWhiteList := appCtx.Config().IpWhiteList
 	ips := strings.Split(strings.ReplaceAll(ipWhiteList, " ", ""), ",")
 	return func(context *gin.Context) {
 		ip := context.ClientIP()
 		claims := context.MustGet(ClaimsKey).(dto.ThkClaims)
 		if !isIpValid(ip, ips) {
-			logger.WithFields(logrus.Fields(claims)).Errorf("RemoteAddr forbidden: %s %v", ip, ips)
+			appCtx.Logger().WithFields(logrus.Fields(claims)).Errorf("RemoteAddr forbidden: %s %v", ip, ips)
 			dto.ResponseForbidden(context)
 			context.Abort()
 		} else {
-			logger.WithFields(logrus.Fields(claims)).Infof("RemoteAddr: %s", ip)
+			appCtx.Logger().WithFields(logrus.Fields(claims)).Infof("RemoteAddr: %s", ip)
 			context.Next()
 		}
 	}
